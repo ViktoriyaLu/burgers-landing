96def29b0598511a530b2b2064de73df9d2f9a14
ViktoriyaLu <114606124+ViktoriyaLu@users.noreply.github.com>
2023-08-21 20:03:26 +0300

    New one

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,17 @@
 //document.getElementsByClassName("main-title")[0].style.color = 'red';  //Заголовок красного цвета
 
-//document.getElementsById("main-action-button").onclick = function () { //обработчик события - нажатие на кнопку
-//document.getElementsById("products").scrollIntoView({behavior: "smooth"});}
+//обработчик события - нажатие на кнопку
 document.getElementById("main-action-button").onclick = function()   {
     document.getElementById("products").scrollIntoView({behavior:"smooth"});
-    
 }
+//плавный скрол до секции при нажатии на кнопку меню
 let links = document.querySelectorAll(".menu-item > a");
 for (let i = 0; i < links.length; i++) {
     links[i].onclick = function () {
         document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior:"smooth"});
     }
-
-
 }
+//переход к форме заплнения при нажатии на кнопку Товара "Заказать"
 let buttons = document.getElementsByClassName("product-button");
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
@@ -21,29 +19,30 @@ for (let i = 0; i < buttons.length; i++) {
     }
 }
 
-
-let burger = document.getElementById('burger');
-let user = document.getElementById('user');
-let phone = document.getElementById('phone');
-document.getElementsById('order-action').onclick = function() {
+//простая валидация формы на запись
+let burger = document.getElementById("burger");
+let user = document.getElementById("user");
+let phone = document.getElementById("phone");
+document.getElementsById("order-action").onclick = function() {
     let hasError = false;
         [burger, user, phone].forEach(item => {
         if (!item.value) {
-            item.parentElement.style.background = 'red';
+            item.parentElement.style.background = "red";
             hasError = true;
         } else {
-            item.parentElement.style.background = " ";
+            item.parentElement.style.background = "";
         }
     });
-
-    if (!hasError) {
+        if (!hasError) {
         [burger, user, phone].forEach(item => {
-            item.value = " ";
+            item.value = "";
         });
-        alert('Спасибо за заказ! мы скоро свяжемся с Вами!');
+        alert("Спасибо за заказ! мы скоро свяжемся с Вами!");
     }
 }
-let prices = document.getElementsByClassName('products-item-price');
+
+// смена валюты и пересчет цены
+let prices = document.getElementsByClassName("products-item-price");
 document.getElementById("change-currency").onclick = function (e) {
     let currentCurrency = e.target.innerText;
 
@@ -57,12 +56,14 @@ document.getElementById("change-currency").onclick = function (e) {
         newCurrency = "BYN";
         coefficient = 3;
     }
+    else if (currentCurrency === "BYN" ) {
+        newCurrency = "€";
+        coefficient = 33;
+    }
     e.target.innerText = newCurrency;
     for (let i = 0; i < prices.lengts; i++) {
         prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
     }
-
-
 }
 
 
